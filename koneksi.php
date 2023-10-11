8207f674e24545ec3bfcd204041e98f500c3f217
montionprojectr <montionprojectr@gmail.com>
2023-10-11 15:20:38 +0700

    perubahan koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$db_name = "db_raport";
+$db_name = "smkg6671_eraportsapra2";
 
 $koneksi = mysqli_connect($host, $username, $password, $db_name) or die("Error Connection : " . mysqli_errno());
 
