c6c648fe11740443540e921ea542a0a8e4c7b0e7
montionprojectr <montionprojectr@gmail.com>
2023-10-11 16:41:05 +0700

    height

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ require_once("koneksi.php");
 <body class="hold-transition login-page bg-image">
 <div class="login-box bg-warning elevation-1 card">
   <img src="dist/img/logo_sapra2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+           style="opacity: .8" height="100px">
   <div class="login-logo">
     <a href="index2.html"><b>E-Raport SAPRA2</b></a>
   </div>
