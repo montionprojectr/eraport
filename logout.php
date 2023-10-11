ab2f1f696db0002940323456efa7ceb98e9925d8
montionprojectr <montionprojectr@gmail.com>
2023-10-11 16:12:12 +0700

    target logout

--- logout.php
+++ logout.php
@@ -4,5 +4,5 @@ unset($_SESSION['session']);
 unset($_SESSION['nama_lengkap']);
 //atau
 session_destroy();
-header("location: ../eraport");
+header("location: https://eraport.smksatyapraja2.id/");
 ?>
