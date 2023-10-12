05f3a99488855fd70b77b89c0ed149413b915e97
montionprojectr <montionprojectr@gmail.com>
2023-10-12 15:32:54 +0700

    koneksi lagi

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,14 @@
 <?php 
-// $host = "localhost";
-// $username = "smkg6671_eraport";
-// $password = "eraportsapra2";
-// $db_name = "smkg6671_eraportsapra2";
-
-
 $host = "localhost";
-$username = "root";
-$password = "";
-$db_name = "db_rap";
+$username = "smkg6671_eraport";
+$password = "eraportsapra2";
+$db_name = "smkg6671_eraportsapra2";
+
+
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $db_name = "db_rap";
 
 $koneksi = mysqli_connect($host, $username, $password, $db_name) or die("Error Connection : " . mysqli_errno());
 
